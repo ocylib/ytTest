da00477666d9c750429ea7524f341003c768aa47
OCY <zichengou@gmail.com>
2022-09-21 17:11:33 +0800

    file to webhook

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const yt = require('ytcog');
-const fs = require('fs');
-
+import yt from 'ytcog';
+import got from 'got';
 (async () => {
 	const session = new yt.Session();
+	const url =
+		'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c6f3ebeb-a18b-4ca1-8622-1adab41cb6c3';
+	const msg = {
+		msgtype: 'markdown',
+		markdown: {
+			content: '出错了',
+		},
+	};
 	await session.fetch();
-	const writer = fs.createWriteStream('log.txt', { flags: 'a' });
 	if (session.status === 'OK') {
-		const log = Date() + ' ' + session.playerUrl + '\n';
-		writer.write(log);
-	} else {
-		writer.write('fail……\n');
+		const { key, sts, id_token, session_token, playerUrl } = session;
+		msg.markdown.content = `\n>key: ${key}\n>sts: ${sts}\n>id_token: ${id_token}\n>session_token: ${session_token}\n>playerUrl: ${playerUrl}`;
 	}
-	writer.end();
+	await got.post(url, { json: msg });
 })();
